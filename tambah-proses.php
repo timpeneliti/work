44ba2183d76751a2e5948f10944780a9ede585ea
adezayd <120370323+adezayd@users.noreply.github.com>
2023-12-30 07:37:47 +0700

    Update tambah-proses.php

--- tambah-proses.php
+++ tambah-proses.php
@@ -1,23 +1,34 @@
 <?php
-if(isset($_POST['tambah'])){
-	include('koneksi.php');
-// Tanpa SQL Injection
-//	$_1		= $_POST['_a'];
-//	$_2		= $_POST['_b'];
-// mencegah	SQL Injection.
-	$_1		= mysqli_real_escape_string($koneksi,$_POST['_a']);
-	
-	$input	= mysqli_query($koneksi, "INSERT INTO a VALUES(0, '$_1')") or die(mysqli_error());
-	if($input){
-		echo 'Data berhasil di tambahkan!';
-		echo '<a href="tambah.php">Kembali</a>';
-	}
-	else{
-		echo 'Gagal menambahkan data!';
-		echo '<a href="tambah.php">Kembali</a>';
-	}
-}
-else{
-	echo '<script>window.history.back()</script>';
+if (isset($_POST['tambah'])) {
+    include('koneksi.php'); // Ensure this file contains your database connection setup
+
+    // Prevent SQL Injection by escaping special characters
+    $_1 = mysqli_real_escape_string($koneksi, $_POST['_a']);
+
+    try {
+        // Check if the connection is still alive; if not, reconnect
+        if (!mysqli_ping($koneksi)) {
+            mysqli_close($koneksi);
+            include('koneksi.php'); // Reconnect by including the connection file again
+        }
+
+        // Prepare and execute the query
+        $query = "INSERT INTO a VALUES(0, '$_1')";
+        $input = mysqli_query($koneksi, $query);
+
+        // Check if the query was successful
+        if ($input) {
+            echo 'Data berhasil ditambahkan!';
+            echo '<a href="tambah.php">Kembali</a>';
+        } else {
+            throw new Exception(mysqli_error($koneksi)); // Throw an exception with the error message
+        }
+    } catch (Exception $e) {
+        // Handle the exception gracefully
+        echo 'Gagal menambahkan data! Error: ' . $e->getMessage();
+        echo '<a href="tambah.php">Kembali</a>';
+    }
+} else {
+    echo '<script>window.history.back()</script>';
 }
 ?>
